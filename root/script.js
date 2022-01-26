5a00ae3cb019b3c0f7cf4c1a595a38254a07bc2b
Adam Šáfr <adam.safr@seznam.cz>
2022-01-26 18:07:49 +0100

    desktop verze, animace

---
+++ root/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var typed = new Typed(".typing", {
+        strings: ["Azul DZ330X"],
+        typeSpeed: 100,
+        backSpeed: 70,
+        loop: true
+    });
+});
